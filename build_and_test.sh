37979c279878aec0d575a86e90e23e7a3d661712
Semenov Fedor <fedor.v.semenov@gmail.com>
2019-12-16 18:31:38 +0500

    Fixed build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -7,7 +7,7 @@ fi
 uname -a
 $CXX --version
 
-mkdir -p build && cd build
-    && cmake -G Ninja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release ${CMAKE_OPTIONS} ..
-    && cmake --build . && ctest --output-on-failure
+mkdir -p build && cd build \
+    && cmake -G Ninja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release ${CMAKE_OPTIONS} .. \
+    && cmake --build . && ctest --output-on-failure \
     && cd ..
