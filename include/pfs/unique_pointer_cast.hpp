c55d0a4b15c58ccebcf141ffdf39e8828b83cfbb
Semenov Fedor <fedor.v.semenov@gmail.com>
2020-04-03 09:31:22 +0500

    Added static_unique_pointer_cast.

---
+++ include/pfs/unique_pointer_cast.hpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2020 Vladislav Trifochkin
+//
+// This file is part of [pfs-common](https://github.com/semenovf/pfs-common) library.
+//
+// Changelog:
+//      2020.04.03 Initial version
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+namespace pfs {
+
+// [How to static_cast std::unique_ptr](https://www.ficksworkshop.com/blog/how-to-static-cast-std-unique-ptr)
+// [Alternatives of static_pointer_cast for unique_ptr](https://stackoverflow.com/questions/36120424/alternatives-of-static-pointer-cast-for-unique-ptr)
+
+template <typename T, typename U>
+unique_ptr<T> static_unique_pointer_cast (unique_ptr<U> && r)
+{
+    return unique_ptr<T>{static_cast<T*>(r.release())};
+}
+
+} // namespace pfs
