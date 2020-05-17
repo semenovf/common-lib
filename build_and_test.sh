da9289fe11c49591592be7203bfb31592cbb581a
Semenov Fedor <fedor.v.semenov@gmail.com>
2020-05-17 13:16:24 +0500

    Fixed build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -8,6 +8,8 @@ uname -a
 $CXX --version
 
 mkdir -p build && cd build \
-    && cmake -G Ninja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release ${CMAKE_OPTIONS} .. \
+    && cmake -G Ninja -DCMAKE_CXX_STANDARD=${CXX_STANDARD} \
+            -Dpfs_common_BUILD_TESTS=ON \
+            -DCMAKE_BUILD_TYPE=Release ${CMAKE_OPTIONS} .. \
     && cmake --build . && ctest --output-on-failure \
     && cd ..
