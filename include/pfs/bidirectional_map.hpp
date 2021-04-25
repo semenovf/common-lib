f17af4572416b2c04edaaf0cfb3ed1f14636205c
Semenov Fedor <fedor.v.semenov@gmail.com>
2021-04-25 17:44:27 +0500

    Minor changes.

---
+++ include/pfs/bidirectional_map.hpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021 Vladislav Trifochkin
+//
+// This file is part of [pfs-common](https://github.com/semenovf/pfs-common) library.
+//
+// Changelog:
+//      2021.02.13 Initial version
+//
+// TODO
+//      1. Implement (see boost::bimap - https://www.boost.org/doc/libs/1_75_0/libs/bimap/doc/html/index.html)
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+namespace pfs {
+
+class bidirectional_map
+{
+
+};
+
+} // namespace pfs
