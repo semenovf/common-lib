fcdfddd39592092173874705e859f746a6293803
Semenov Fedor <fedor.v.semenov@gmail.com>
2021-12-17 19:32:44 +0500

    feat: Added macros `PFS_PLATFORM_LITERAL` and `PFS_UTF8_ENCODE_PATH`.

--- include/pfs/filesystem.hpp
+++ include/pfs/filesystem.hpp
@@ -10,6 +10,10 @@
 #pragma once
 #include "bits/compiler.h"
 
+#if defined(PFS_COMPILER_MSVC)
+#   include "windows.hpp"
+#endif
+
 // NOTE. Visual Studio supports the new C++17 <filesystem> standard since
 // Visual Studio 2017 version 15.7 (_MSC_VER >= 1914, _MSC_FULL_VER >= 191426428).
 
@@ -20,7 +24,7 @@
 #       define PFS_HAVE_STD_FILESYSTEM 1
 #   endif
 #endif
-        
+
 #if PFS_HAVE_STD_FILESYSTEM
 #   include <filesystem>
 namespace pfs {
@@ -28,12 +32,31 @@ namespace filesystem {
     using namespace std::filesystem;
 }}
 #else
-#   if defined(_MSC_VER)
-#       define GHC_WIN_WSTRING_STRING_TYPE
-#   endif
 #   include "3rdparty/ghc/filesystem.hpp"
 namespace pfs {
 namespace filesystem {
     using namespace ghc::filesystem;
 }} // namespace pfs::filesystem
+#endif
+
+#ifndef PFS_PLATFORM_LITERAL
+#   if defined(PFS_COMPILER_MSVC)
+#       if !(defined(_UNICODE) || defined(UNICODE))
+#           error "Expected _UNICODE(UNICODE) is enabled"
+#       endif
+#       define PFS_PLATFORM_LITERAL(x) L##x
+#   else
+#       define PFS_PLATFORM_LITERAL(x) x
+#   endif
+#endif
+
+#ifndef PFS_UTF8_ENCODE_PATH
+#   if defined(PFS_COMPILER_MSVC)
+#       if !(defined(_UNICODE) || defined(UNICODE))
+#           error "Expected _UNICODE(UNICODE) is enabled"
+#       endif
+#       define PFS_UTF8_ENCODE_PATH(x) utf8_encode(x)
+#   else
+#       define PFS_UTF8_ENCODE_PATH(x) x
+#   endif
 #endif
