950eb50f528f6d9af15bad4534133cf6ca614e52
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-06-15 15:52:49 +0500

    feat: Implemented `qHash` for `std::string`.

--- include/pfs/qt_compat.hpp
+++ include/pfs/qt_compat.hpp
@@ -29,3 +29,15 @@ struct formatter<QString>
 };
 
 } // namespace fmt
+
+#include <QByteArray>
+#include <QHash>
+
+namespace std {
+
+inline unsigned int qHash (std::string const & key, unsigned int seed = 0)
+{
+    return qHash(QByteArray::fromRawData(key.data(), key.length()), seed);
+}
+
+} // namespace std
