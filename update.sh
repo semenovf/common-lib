1b62cc77f72f2c0c3313d283731f742bdb741feb
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 13:27:33 +0500

    Updated `update.sh`.

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+CWD=`pwd`
+
 if [ -d .git ] ; then
 
     git pull \
         && git submodule update --init \
-        && cd 3rdparty/portable-target && git pull && git checkout master
+        && cd 3rdparty/portable-target && git pull && git checkout master \
+        && cd $CWD
 
 fi
 
