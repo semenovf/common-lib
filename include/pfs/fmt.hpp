bfbf1e397be8304051947e53c33b6b6d99efdafb
Vladislav Trifochkin <demoversion@mail.ru>
2025-04-26 11:51:19 +0500

    fix: Fixed int128 support check for use with `fmt`.

--- include/pfs/fmt.hpp
+++ include/pfs/fmt.hpp
@@ -7,6 +7,14 @@
 //      2019.12.23 Initial version.
 ////////////////////////////////////////////////////////////////////////////////
 #pragma once
+#include "i128.hpp"
+
+#if PFS_HAS_INT128
+#   ifndef FMT_USE_INT128
+#       define FMT_USE_INT128 1
+#   endif
+#endif
+
 #define FMT_HEADER_ONLY 1
-#include "pfs/3rdparty/fmt/format.h"
-#include "pfs/3rdparty/fmt/chrono.h"
+#include "3rdparty/fmt/format.h"
+#include "3rdparty/fmt/chrono.h"
